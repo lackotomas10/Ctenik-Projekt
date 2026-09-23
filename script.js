b4af6604a26dc2bef7e73e36c23d7267c7ebbb6e
lackotomas10 <lackotomas.10@gmail.com>
2026-09-23 21:26:15 +0200

    JS, nový obrázek

--- script.js
+++ script.js
@@ -7,13 +7,27 @@ document.getElementById('Datum').value = `${yyyy}-${mm}-${dd}`;
 
 let ZáznDatum = `${yyyy}-${mm}-${dd}`
 let Minuty = 0
-let Dočtení
+let Dočtení = 0
+let Přihlášení = false
+
+Aktualizace()
 
 function Záznam(){
     ZáznDatum = document.getElementById("Datum").value //čtení zadaných hodnot funguje, přidat databázi a práci s hodnotami
     Minuty = document.getElementById("MinČtení").value // jako funkce která aktualizuje stat. a přehled
     Dočtení = document.getElementById("Dočtení").value
 
-
-
 }
+
+function Přihlásit(){} //Dodělat později
+
+function Aktualizace(){
+    if (Přihlášení == false){
+        document.getElementById("AlarmPřihlášení").innerText = "Nejste přihlášni, většina funkcí nebude aktivní"
+        document.getElementById("AlarmPřihlášení").style.color = "red"
+        document.getElementById("AlarmPřihlášení").style.backgroundColor = "rgb(255, 151, 151)"
+        document.getElementById("AlarmPřihlášení").style.borderRadius = "5px"
+
+    }
+}
+
